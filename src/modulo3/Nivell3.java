ccee4b0608963ecfdb92052fbbfc406743341b07
FabianaArzamendia <67900644+FabianaArzamendia@users.noreply.github.com>
2021-07-03 00:48:02 +0200

    Update Nivell3.java
    Borrado de comentarios innecesarios.

--- src/modulo3/Nivell3.java
+++ src/modulo3/Nivell3.java
@@ -2,6 +2,7 @@ package modulo3;
 
 import java.util.*;
 
+//FabianaArzamendia
 public class Nivell3 {
 	
 	public static void main(String[] args) {
@@ -17,16 +18,11 @@ public class Nivell3 {
 		 int n2 = 0;
 		
 		 System.out.print(n0 + " " + n1);
-		 for(int i =0 ; i < num -2 ; i++) {//num 7 -2 = 5 ira hasta 5 el bucle 
-			 n2 = n0 + n1; // 0 1   n2 = 1 
-			 /* i = 0  n2 =1
-			  * i = 1  n3 =2
-			  * i = 2  n4 =3
-			  * i = 3  n5 =5
-			  * i = 4  n6 =8
-			  */
-			 n0 = n1;//n0 1  
-			 n1 = n2;//n1 1
+		 for(int i =0 ; i < num -2 ; i++) {
+			 n2 = n0 + n1; 
+			
+			 n0 = n1;
+			 n1 = n2;
 			 
 			 System.out.print(" "  + n2 + " ");
 			 
